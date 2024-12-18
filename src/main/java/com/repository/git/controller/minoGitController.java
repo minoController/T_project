d74c3940558add8e65e8bf449db96cf2d933d4d4
mino01 <test@buttle.co.kr>
2024-12-18 11:04:14 +0900

    mino_git create Controller

---
+++ src/main/java/com/repository/git/controller/minoGitController.java
@@ -0,0 +1,9 @@
+package com.repository.git.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class minoGitController {
+
+    // mino_git fix it
+}
